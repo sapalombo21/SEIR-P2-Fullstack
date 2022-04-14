3810dff858907388a9cff53f5f909064754e5f27
sapalombo21 <sam.palombo.2001@gmail.com>
2022-04-14 12:06:22 -0500

    user profile controller

---
+++ controller/user.js
@@ -0,0 +1,10 @@
+const User = require('../models/user');
+const Review = require('../models/review');
+const Game = require('../models/game');
+
+module.exports = {show}
+
+async function show(req, res) {
+    const reviews = await Review.find({user: req.params.id})
+    res.render('user/show', {title: "User Reviews", reviews});
+}
