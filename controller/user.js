583755dc4416dee7c028ec0951feceef710f2df9
sapalombo21 <sam.palombo.2001@gmail.com>
2022-04-14 12:23:28 -0500

    formatting

--- controller/user.js
+++ controller/user.js
@@ -1,10 +1,10 @@
-const User = require('../models/user');
-const Review = require('../models/review');
-const Game = require('../models/game');
+const User = require("../models/user");
+const Review = require("../models/review");
+const Game = require("../models/game");
 
-module.exports = {show}
+module.exports = { show };
 
 async function show(req, res) {
-    const reviews = await Review.find({user: req.params.id})
-    res.render('user/show', {title: "User Reviews", reviews});
+  const reviews = await Review.find({ user: req.params.id });
+  res.render("user/show", { title: "User Reviews", reviews });
 }
