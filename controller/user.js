0d4b29ff4470d8b18ff65bd1b7816818530df97b
sapalombo21 <sam.palombo.2001@gmail.com>
2022-04-15 13:44:24 -0500

    user profile fleshed out, makes more sense

--- controller/user.js
+++ controller/user.js
@@ -5,6 +5,6 @@ const Game = require("../models/game");
 module.exports = { show };
 
 async function show(req, res) {
-  const reviews = await Review.find({ user: req.params.id }).populate('game').exec();
-  res.render("user/show", { title: "User Reviews", reviews });
+  const reviews = await Review.find({ user: req.params.id }).populate('game').populate('user').exec();
+  res.render("user/show", { title: `${reviews[0].user.name}`, reviews });
 }
